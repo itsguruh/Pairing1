737be499284b7d6e361bc27e3a74b1dd2e0323e3
GURU <cryptixmd@gmail.com>
2025-09-14 16:48:26 +0300

    CRYPTIX-MD

--- ping.js
+++ ping.js
@@ -1,1 +1,19 @@
+const http = require('http');
 
+const url = process.env.HEROKU_APP_URL; // set in Config Vars
+const interval = 25 * 60 * 1000; // 25 minutes
+
+if (!url) {
+  console.error("HEROKU_APP_URL not set in Config Vars");
+  process.exit(1);
+}
+
+setInterval(() => {
+  http.get(url, (res) => {
+    console.log(`Pinged ${url} - Status: ${res.statusCode}`);
+  }).on('error', (err) => {
+    console.error('Ping error:', err.message);
+  });
+}, interval);
+
+console.log(`Ping bot started. Pinging every 25 minutes: ${url}`);
