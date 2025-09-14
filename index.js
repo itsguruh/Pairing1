6466d2fd6a7100e0115a005457ce6ce367254f99
GURU <cryptixmd@gmail.com>
2025-09-14 17:13:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star CASEYRHODES-XMD
+Don't Forget To Give Star CRYPTIX MD
 
  Server running on http://localhost:` + PORT)
 })
