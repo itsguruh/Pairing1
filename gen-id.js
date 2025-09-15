a9b9fbdab2f805ea09d1e921ab02a9264d07c946
GURU <cryptixmd@gmail.com>
2025-09-15 13:54:54 +0300

    CRYPTIX-MD

--- gen-id.js
+++ gen-id.js
@@ -1,10 +1,12 @@
 function makeid(num = 4) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
+  const charactersLength = characters.length;
+  for (let i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
-module.exports = {makeid};
+
+// ✅ Export as object
+module.exports = { makeid };
